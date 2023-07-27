e990091ffafb530325dd2609fe2a392cd64364b9
Serhii-Sunko <sergeysunko01@gmail.com>
2023-07-27 21:01:22 +0300

    added close modal option with click on btn|| escape code|| backdrope

--- react-4-lesson/src/components/modal/Modal.js
+++ react-4-lesson/src/components/modal/Modal.js
@@ -3,10 +3,31 @@ import { ModalContainer } from "./ModalStyled";
 
 class Modal extends Component {
   state = {};
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.closeModal);
+    const body = document.querySelector("body");
+    body.style.overflow = "hidden";
+  }
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.closeModal);
+    const body = document.querySelector("body");
+    body.style.overflow = "auto";
+  }
+  closeModal = (e) => {
+    if (e.code === "Escape") {
+      this.props.toogleModal();
+    }
+  };
+  onHandleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      this.props.toggleModal();
+    }
+  };
   render() {
     const { children, toggleModal } = this.props;
     return (
-      <ModalContainer>
+      <ModalContainer onClick={this.onHandleBackdropClick}>
         <div className="Modal">
           {children}
           <button type="button" className="modalBtn" onClick={toggleModal}>
